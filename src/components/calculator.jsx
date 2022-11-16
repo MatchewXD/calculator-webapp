60caf4508e91a4cbfd8556267d17a804292ce516
Matthew Seagren <mattseagren@gmail.com>
2022-11-16 01:01:03 -0800

    Removed React info. Created calculator design on figma. Started recreating design through css

---
+++ src/components/calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return <div className="webpage">
+      <div className="business-card"></div>
+    </div>;
+  }
+}
+
+export default Calculator
