04ff7c0239c04f8c776defa55892c1ee748ce5b3
Matthew Seagren <mattseagren@gmail.com>
2022-11-18 20:59:28 -0800

    Finally got the calc frame, eyes and business card to be responsive to all devices. Now I need to work on the chat bubble

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      github: "https://github.com/MatchewXD",
+      linkedin: "https://www.linkedin.com/in/matthew-seagren/"
+    }
+
+    // this.renderView = this.renderView.bind(this)
+  }
   render() {
     return <div className="webpage">
-      <div className="business-card"></div>
-    </div>;
+      <div className="business-card">
+        <div className="matchewxd">MatchewXD</div>
+        <div className="job-disc">Software Engineer<br></br>"Live Better Together"<br></br>GitHub<br></br>LinkedIn</div>
+        {/* <div className="linx">GitHub<br></br>LinkedIn</div> */}
+      </div>
+      <div className="calc-container">
+        <div className="eye-container">
+          <div className="left eye"></div>
+          <div className="right eye"></div>
+        </div>
+        <div className="calc"></div>
+      </div>
+    </div >;
   }
 }
 
