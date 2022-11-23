6e2b45ed69e7d164c5b570713134e190ede65195
Matthew Seagren <mattseagren@gmail.com>
2022-11-22 21:17:05 -0800

    Added calculator buttons and text for the buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,7 +4,66 @@ import '../App.css';
 function Buttons() {
   return (
     <div className="buttons-frame">
-
+      <div className="calc-button left-paratheses">
+        <p className="button-symbol">(</p>
+      </div>
+      <div className="calc-button right-paratheses">
+        <p className="button-symbol">)</p>
+      </div>
+      <div className="calc-button percent">
+        <p className="button-symbol">%</p>
+      </div>
+      <div className="calc-button ac">
+        <p className="button-symbol">AC</p>
+      </div>
+      <div className="calc-button seven">
+        <p className="button-symbol">7</p>
+      </div>
+      <div className="calc-button eight">
+        <p className="button-symbol">8</p>
+      </div>
+      <div className="calc-button nine">
+        <p className="button-symbol">9</p>
+      </div>
+      <div className="calc-button slash">
+        <p className="button-symbol">/</p>
+      </div>
+      <div className="calc-button four">
+        <p className="button-symbol">4</p>
+      </div>
+      <div className="calc-button five">
+        <p className="button-symbol">5</p>
+      </div>
+      <div className="calc-button six">
+        <p className="button-symbol">6</p>
+      </div>
+      <div className="calc-button astrix">
+        <p className="button-symbol">*</p>
+      </div>
+      <div className="calc-button one">
+        <p className="button-symbol">1</p>
+      </div>
+      <div className="calc-button two">
+        <p className="button-symbol">2</p>
+      </div>
+      <div className="calc-button three">
+        <p className="button-symbol">3</p>
+      </div>
+      <div className="calc-button minus">
+        <p className="button-symbol">-</p>
+      </div>
+      <div className="calc-button zero">
+        <p className="button-symbol">0</p>
+      </div>
+      <div className="calc-button period">
+        <p className="button-symbol">.</p>
+      </div>
+      <div className="calc-button equals">
+        <p className="button-symbol">=</p>
+      </div>
+      <div className="calc-button plus">
+        <p className="button-symbol">+</p>
+      </div>
     </div>
   );
 }
