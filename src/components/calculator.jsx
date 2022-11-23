a2f2454c8b081a02c803d457ad44087760c6f389
Matthew Seagren <mattseagren@gmail.com>
2022-11-23 12:04:54 -0800

    Working on calculator functionality. All buttons have a function now except wake and they all effect the output field. What I am doing now is making them actually calulate accurately

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -19,9 +19,154 @@ class Calculator extends React.Component {
   }
 
   buttonClick(e) {
-    // this.state(output: "0");
     e.preventDefault();
-    console.log(e.target.textContent);
+
+    var cSymbol = e.target.textContent;
+    var outp = this.state.output;
+
+    function equals(str) {
+      var isLeft = true;
+      var leftOp = '';
+      var rightOp = '';
+      var cOp = '';
+      var operands = ['+', '-', '*', '/', '%'];
+
+
+      for (var i = 0; i < str.length; i++) {
+        if (operands.includes(str[i])) {
+          if (!isLeft) {
+            debugger;
+            leftOp = Number(leftOp);
+            rightOp = Number(rightOp);
+
+            if (cOp === '+') {
+              leftOp = leftOp + rightOp;
+            } else if (cOp === '-') {
+              leftOp = leftOp - rightOp;
+            } else if (cOp === '*') {
+              leftOp = leftOp * rightOp;
+            } else if (cOp === '/') {
+              leftOp = leftOp / rightOp;
+            }
+
+            leftOp = leftOp.toString();
+            rightOp = '';
+            cOp = str[i];
+          } else {
+            cOp = str[i];
+            isLeft = false;
+          }
+
+        } else {
+          if (isLeft) {
+            leftOp += str[i];
+          } else {
+            rightOp += str[i];
+          }
+        }
+      }
+
+      leftOp = Number(leftOp);
+      rightOp = Number(rightOp);
+
+      if (cOp === '+') {
+        return leftOp + rightOp;
+      } else if (cOp === '-') {
+        return leftOp - rightOp;
+      } else if (cOp === '*') {
+        return leftOp * rightOp;
+      } else if (cOp === '/') {
+        return leftOp / rightOp;
+      } else {
+        console.log('Operand is not added yet');
+      }
+    }
+
+    function calculate(sym) {
+      var isZero = true;
+      if (outp !== '0') {
+        isZero = false;
+      }
+
+      function checkZero(symb) {
+        if (isZero) {
+          outp = symb;
+        } else {
+          outp += symb;
+        }
+      }
+
+      if (sym === 'AC') {
+        console.log('Button pressed is AC');
+        outp = '0';
+      } else if (sym === "(") {
+        console.log('Button pressed is (');
+        checkZero('(');
+      } else if (sym === ")") {
+        console.log('Button pressed is )');
+        checkZero(')');
+      } else if (sym === "%") {
+        console.log('Button pressed is %');
+        // checkZero('%');
+        outp += '%';
+      } else if (sym === "7") {
+        console.log('Button pressed is 7');
+        checkZero('7');
+      } else if (sym === "8") {
+        console.log('Button pressed is 8');
+        checkZero('8');
+      } else if (sym === "9") {
+        console.log('Button pressed is 9');
+        checkZero('9');
+      } else if (sym === "/") {
+        console.log('Button pressed is /');
+        // checkZero('/');
+        outp += ' / ';
+      } else if (sym === "4") {
+        console.log('Button pressed is 4');
+        checkZero('4');
+      } else if (sym === "5") {
+        console.log('Button pressed is 5');
+        checkZero('5');
+      } else if (sym === "6") {
+        console.log('Button pressed is 6');
+        checkZero('6');
+      } else if (sym === "*") {
+        console.log('Button pressed is *');
+        outp += ' * ';
+      } else if (sym === "1") {
+        console.log('Button pressed is 1');
+        checkZero('1');
+      } else if (sym === "2") {
+        console.log('Button pressed is 2');
+        checkZero('2');
+      } else if (sym === "3") {
+        console.log('Button pressed is 3');
+        checkZero('3');
+      } else if (sym === "-") {
+        console.log('Button pressed is -');
+        outp += ' - ';
+      } else if (sym === "0") {
+        console.log('Button pressed is 0');
+        checkZero('0');
+      } else if (sym === ".") {
+        console.log('Button pressed is .');
+        outp += '.';
+      } else if (sym === "=") {
+        console.log('Button pressed is =');
+        outp = outp.split(' ').join('');
+        // console.log(outp);
+        outp = equals(outp);
+      } else if (sym === "+") {
+        console.log('Button pressed is +');
+        outp += ' + ';
+      } else {
+        console.log('Button is not logged');
+      }
+    }
+    calculate(cSymbol);
+
+    this.setState({ output: outp });
   }
 
   render() {
